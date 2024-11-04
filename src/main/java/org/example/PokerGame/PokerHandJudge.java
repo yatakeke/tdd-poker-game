4ad5627d3dd446dd91e879906e08e99d0e60619a
yatakeke <yatasunshine.1456@gmail.com>
2024-11-04 12:27:40 +0900

    can judge high card

--- src/main/java/org/example/PokerGame/PokerHandJudge.java
+++ src/main/java/org/example/PokerGame/PokerHandJudge.java
@@ -1,11 +1,19 @@
 package org.example.PokerGame;
 
+import java.util.ArrayList;
+
 public class PokerHandJudge {
 
     public PokerHand judge(Player player) {
+        var numbers = new ArrayList<Integer>();
         for (var card: player.allCard()) {
-
+            if (numbers.contains(card.i())) {
+                return PokerHand.ONE_PAIR;
+            }
+            else {
+                numbers.add(card.i());
+            }
         }
-        return PokerHand.ONE_PAIR;
+        return PokerHand.HIGH_CARD;
     }
 }
