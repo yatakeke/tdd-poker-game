ca5b998041531503e06b2eae37ca256633ebeec3
yatakeke <yatasunshine.1456@gmail.com>
2024-11-04 12:32:36 +0900

    added setUp method

--- src/test/java/org/example/PokerGame/PokerHandJudgeTest.java
+++ src/test/java/org/example/PokerGame/PokerHandJudgeTest.java
@@ -1,22 +1,29 @@
 package org.example.PokerGame;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.google.common.truth.Truth.assertThat;
 
 class PokerHandJudgeTest {
 
+    private PokerHandJudge pokerHandJudge;
+
+    @BeforeEach
+    void setUp() {
+        this.pokerHandJudge = new PokerHandJudge();
+    }
+
     @Test
     public void testOnePair() {
-        Player Player = new Player(
+        Player player = new Player(
                 new Card(1, CardMark.SPADE),
                 new Card(1, CardMark.CLUB),
                 new Card(2, CardMark.HEART),
                 new Card(3, CardMark.DIAMOND),
                 new Card(4, CardMark.SPADE)
         );
-        var result = new PokerHandJudge().judge(Player);
-        assertThat(result).isEqualTo(PokerHand.ONE_PAIR);
+        assertThat(pokerHandJudge.judge(player)).isEqualTo(PokerHand.ONE_PAIR);
     }
 
     @Test
@@ -28,8 +35,6 @@ class PokerHandJudgeTest {
                 new Card(4, CardMark.CLUB),
                 new Card(5, CardMark.HEART)
         );
-
-        var result = new PokerHandJudge().judge(player);
-        assertThat(result).isEqualTo(PokerHand.HIGH_CARD);
+        assertThat(pokerHandJudge.judge(player)).isEqualTo(PokerHand.HIGH_CARD);
     }
 }
