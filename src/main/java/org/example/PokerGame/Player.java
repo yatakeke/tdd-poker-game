a1cf7c21c243fc27ba4a11875db74b6fc7674cf7
yatakeke <yatasunshine.1456@gmail.com>
2024-11-04 12:24:18 +0900

    can get player card

--- src/main/java/org/example/PokerGame/Player.java
+++ src/main/java/org/example/PokerGame/Player.java
@@ -1,7 +1,21 @@
 package org.example.PokerGame;
 
-public class Player {
-    public Player(Card card1, Card card2, Card card3, Card card4, Card card5) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Player {
+    private final ArrayList<Card> allCard;
+
+    public Player(Card card1, Card card2, Card card3, Card card4, Card card5) {
+        this.allCard = new ArrayList<>();
+        allCard.add(card1);
+        allCard.add(card2);
+        allCard.add(card3);
+        allCard.add(card4);
+        allCard.add(card5);
+    }
+
+    public List<Card> allCard() {
+        return this.allCard;
     }
 }
--- src/main/java/org/example/PokerGame/PokerHandJudge.java
+++ src/main/java/org/example/PokerGame/PokerHandJudge.java
@@ -3,6 +3,9 @@ package org.example.PokerGame;
 public class PokerHandJudge {
 
     public PokerHand judge(Player player) {
+        for (var card: player.allCard()) {
+
+        }
         return PokerHand.ONE_PAIR;
     }
 }
